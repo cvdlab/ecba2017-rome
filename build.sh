671c751474dfbf45746f376f516c2be95eaec06c
Christian Vadalà <chr.vadala@gmail.com>
2016-12-15 12:32:01 +0100

    improve docker build

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 # The first time you need to exec `docker build -t texlive:full .`
 
-docker run -it --rm --volume `pwd`:/tmp texlive:full pdflatex bare_conf_compsoc
+docker run -it --rm --volume `pwd`:/tmp texlive:full sh -c "bibtex bare_conf_compsoc && pdflatex bare_conf_compsoc"
